70664d903e489fafbf50c97a234cabede79d1e50
vikyw89 <vikyw89@gmail.com>
2024-04-25 11:56:18 +0800

    chore: savepoint

---
+++ server/src/services/README.md
@@ -0,0 +1,6 @@
+# AVAILABLE SERVICES
+
+
+- predicts (agent / llm layer to generate text)
+- threads (abstraction of back and fort communication with user)
+- 
---
+++ server/src/services/threads/index.py
@@ -0,0 +1,20 @@
+class Thread:
+    def __init__(self, patient_id: str):
+        self.patient_id = patient_id
+        self.thread = None
+        pass
+
+    async def load(self):
+        from src.datasources.prisma import prisma
+
+        self.thread = await prisma.thread.find_first(
+            where={"User": {""}}
+        )
+
+    async def save(self):
+        from src.datasources.prisma import prisma
+
+        self.thread = await prisma.thread.update(
+            where={"User": {"is": {"patientId": self.patient_id}}},
+            data={"Messages":{"set":[]}},
+        )
