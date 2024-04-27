5b9d25fe3164ee6617a35f9c6e25a478ed0a7206
vikyw89 <vikyw89@gmail.com>
2024-04-27 18:20:55 +0800

    feat: streaming ?

--- server/src/services/agents/gemini_chat_agent/index.py
+++ server/src/services/agents/gemini_chat_agent/index.py
@@ -24,14 +24,29 @@ async def run(user_message: str, user_id: str):
     # TODO: bypass user id from agent tool call for security
     inputs = {"messages": [HumanMessage(content=user_message)], "user_id": user_id}
     await prisma.connect()
-    async for output in runnable.astream(inputs):
-        # stream() yields dictionaries with output keyed by node name
-        for key, value in output.items():
-            print(f"Output from node '{key}':")
-            print("---")
-            print(value)
-        print("\n---\n")
-
+    
+    # not streaming
+    # async for output in runnable.astream(inputs):
+    #     # stream() yields dictionaries with output keyed by node name
+    #     for key, value in output.items():
+    #         print(f"Output from node '{key}':")
+    #         print("---")
+    #         print(value)
+    #     print("\n---\n")
+    count = 0
+    async for output in runnable.astream_log(inputs, include_types=["llm"]):
+        count += 1
+        # astream_log() yields the requested logs (here LLMs) in JSONPatch format
+        for op in output.ops:
+            if op["path"] == "/streamed_output/-":
+                # this is the output from .stream()
+                ...
+            elif op["path"].startswith("/logs/") and op["path"].endswith(
+                "/streamed_output/-"
+            ):
+                # because we chose to only include LLMs, these are LLM tokens
+                print("count",count)
+                print(op["value"])
     await prisma.disconnect()
 
 asyncio.run(run(user_id="test", user_message="hello my name is viky, my bp is 80 120, i don't know my body temp, i arrive here by Grab, my heart rate seems normal, my oxygen is 99%"))
