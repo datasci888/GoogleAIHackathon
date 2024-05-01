686609ae923299259d089b95b376d36a9204dfdc
vikyw89 <vikyw89@gmail.com>
2024-05-01 15:53:55 +0800

    feat: buggy streaming

--- server/src/services/agents/gemini_chat_agent/nodes/gemini_model.py
+++ server/src/services/agents/gemini_chat_agent/nodes/gemini_model.py
@@ -2,11 +2,9 @@ from typing import Literal
 from src.configs.index import GOOGLE_API_KEY
 from src.services.agents.gemini_chat_agent.states.index import AgentState
 from langchain_core.messages import HumanMessage, AIMessage
-
+from langchain_google_genai.chat_models import ChatGoogleGenerativeAI
 
 async def gemini_model(state: AgentState):
-    from langchain_google_genai.chat_models import ChatGoogleGenerativeAI
-
     model = ChatGoogleGenerativeAI(model="gemini-pro", google_api_key=GOOGLE_API_KEY)
 
     input = [
@@ -39,24 +37,30 @@ async def run():
     # print("streaming", streaming)
     
     streaming = runnable.astream_log(input={"input_messages": [HumanMessage(content="hello what is MTS ?")]})
+
+    final_text = ""
     async for output in streaming:
         # print("output", output.ops)
-        print("\n")
+        # print("\n")
         for op in output.ops:
-            if op["op"] == 'add':
-                print(op)
-        # for op in output.ops:
-        #     if op["path"] == "/streamed_output/-":
-        #         # this is the output from .stream()
-        #         print("stream",op["value"])
-        #     elif op["path"].startswith("/logs/") and op["path"].endswith(
-        #         "/streamed_output/-"
-        #     ):
-        #         # because we chose to only include LLMs, these are LLM tokens
-        #         print("streamed_output", op["value"])
-    from langchain_core.runnables import RunnableConfig
-
+            # print("op", op)
+            if op["path"].startswith("/logs/ChatGoogleGenerativeAI/streamed_output_str/-"):
+                final_text += op["value"]
+                yield op["value"]
+                print("stream",op["value"])
+            elif op["path"].startswith("/logs/") and op["path"].endswith(
+                "/streamed_output/-"
+            ):
+                # because we chose to only include LLMs, these are LLM tokens
+                # print("streamed_output", op["value"])
+                pass
+    print("funal", final_text)
 
 import asyncio
 
-asyncio.run(run())
+async def test():
+    stream = run()
+    async for output in stream:
+        print(output)
+        
+asyncio.run(test())
